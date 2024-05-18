26e1ba0bfeaabf2b8a872390cc1c3e357f3f336a
Daniel-Tayden Larsen <46882277+MCLegoMan@users.noreply.github.com>
2024-05-19 01:29:00 +1200

    FiveTwo 1.0 - Added Green Apple (This was a test to see how items worked)

--- src/main/java/com/mclegoman/mclmfivetwo/common/FiveTwo.java
+++ src/main/java/com/mclegoman/mclmfivetwo/common/FiveTwo.java
@@ -8,9 +8,15 @@
 package com.mclegoman.mclmfivetwo.common;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.item.FoodItem;
+import net.minecraft.item.Item;
 
 public class FiveTwo implements ModInitializer {
+	public static final Item greenApple;
 	@Override
 	public void onInitialize() {
+	}
+	static {
+		greenApple = new FoodItem(1000, 4, 0.3F, false).setName("mclmfivetwo_green_apple");
 	}
 }
